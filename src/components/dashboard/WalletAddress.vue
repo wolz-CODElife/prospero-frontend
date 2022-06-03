6e3f62ea41534ac1aa60acf1ac11bad190064886
wolz-codelife <joeladewole3@gmail.com>
2022-06-03 18:16:58 +0100

    (Development): Display Wallet address on Sidebar

--- src/components/dashboard/WalletAddress.vue
+++ src/components/dashboard/WalletAddress.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="px-[24px] py-[12px] text-base text-white bg-[#2D3035]">
 		<div>
-			<span class=""> {{ address.splice(0, 4) }}...{{ address.splice(0, 4) }} </span>
+			<span class=""> {{ walletAddress[0] }}...{{ walletAddress[1] }} </span>
 		</div>
 	</div>
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue'
+const props = defineProps({
 	address: {
 		type: String,
 		required: true,
 	},
 });
+
+const walletAddress = computed(() => [props.address.slice(0, 5), props.address.slice(-4)])
 </script>
