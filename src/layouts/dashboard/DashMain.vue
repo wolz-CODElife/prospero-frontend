88fb14b46bcb8b3caa1cd3e982ceb0024bf873e4
Dikachi Nze <nzebenflorence@gmail.com>
2022-06-16 21:11:14 +0100

    dashboard barebone

--- src/layouts/dashboard/DashMain.vue
+++ src/layouts/dashboard/DashMain.vue
@@ -4,18 +4,18 @@
   >
     <div class="col-span-5 grid grid-rows-6 gap-[10px]">
       <!-- Pie chart  -->
-      <div class="row-span-4 bg-[#191A20]">Pie chart</div>
+      <div class="row-span-4 bg-[#191A20]"></div>
 
       <!--Social Media  -->
-      <div class="row-span-2 bg-[#191A20]">Pie chart</div>
+      <div class="row-span-2 bg-[#191A20]"></div>
     </div>
 
     <div class="col-span-7 grid grid-rows-5 gap-[10px]">
       <!-- Line chart  -->
-      <div class="row-span-2 bg-[#191A20]">Pie chart</div>
+      <div class="row-span-2 bg-[#191A20]"></div>
 
       <!--Table  -->
-      <div class="row-span-3 bg-[#191A20]">Pie chart</div>
+      <div class="row-span-3 bg-[#191A20]"></div>
     </div>
   </div>
 </template>
