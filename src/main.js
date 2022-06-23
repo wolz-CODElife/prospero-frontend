2cbe9ef91d74a99cd5f8c5e34fced5b07cb00791
Dikachi Nze <nzebenflorence@gmail.com>
2022-06-23 03:15:37 +0100

    remove import for old module

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/css/tailwind.css";
 import { createPinia } from "pinia";
-// import Notifications from '@kyvg/vue3-notification'
 
 const app = createApp(App);
 
