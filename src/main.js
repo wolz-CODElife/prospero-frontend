004051e38c8b6d74b232c7cc282eb70a39ad191e
Dikachi Nze <nzebenflorence@gmail.com>
2022-06-29 16:22:19 +0100

    fix: api bug

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
-import { initializeApi, rebalance, withdraw, getGraphData, getHistoricalPrices, updateUIFieldValues} from "@/api";
+import {
+	initializeApi,
+	rebalance,
+	withdraw,
+	getGraphData,
+	getHistoricalPrices,
+	updateUIFieldValues,
+} from "@/api";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/tailwind.css";
@@ -13,7 +20,7 @@ await initializeApi();
 //console.log("TESTING")
 //await rebalance([90,10], ["0x9a1a6f9bfb0f93dbec9c0e8ffa36e5628ec681fc","0x03098b801e61f9efb3fdb42676a594bbc587d69d"]);
 //await withdraw([], 1000000000000000000);
-await updateUIFieldValues();
+// await updateUIFieldValues();
 
 //await getGraphData();
 const app = createApp(App);
