43dc5be9ecadc4c2c885031954a5f885b05a208b
Dikachi Nze <nzebenflorence@gmail.com>
2022-07-02 00:11:10 +0100

    optimize smart contract initializations wip

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -2,8 +2,17 @@
 	<div class="grid grid-cols-[300px_1fr] gap-0">
 		<Sidebar class="w-[300px]" />
 		<main class="z-10 bg-[#2D3035] p-[28px] relative">
-			<JoinDepositModal v-if="joinView" @go-back="goBack" />
-			<CreateDepositModal v-else-if="createView" @go-back="goBack" />
+			<JoinDepositModal
+				v-if="joinView"
+				@go-back="goBack"
+				@redirect="redirect"
+			/>
+			<CreateDepositModal
+				v-else-if="createView"
+				@go-back="goBack"
+				@redirect="redirect"
+			/>
+
 			<div v-else>
 				<DashHeader />
 				<DashMain
@@ -17,21 +26,33 @@
 </template>
 
 <script setup>
-import { ref, computed, onBeforeMount } from "vue";
-import { joinPortfolio, createPortfolio } from "@/api";
+import { ref, computed, onBeforeMount, onMounted } from "vue";
+import { joinPortfolio, createPortfolio, deposit } from "@/api";
 import Sidebar from "./dashboard/Sidebar.vue";
 import DashHeader from "./dashboard/DashHeader.vue";
 import DashMain from "./dashboard/DashMain.vue";
 import JoinDepositModal from "@/components/JoinDepositModal.vue";
 import CreateDepositModal from "@/components/CreateDepositModal.vue";
 import { usePortfolios } from "@/stores/Portfolios";
+import { initializeApi } from "@/api";
 
 const portfolioStore = usePortfolios();
 
 // Route protection
-onBeforeMount(() => {
+onBeforeMount(async () => {
 	if (!JSON.parse(localStorage.getItem("userState")).status) {
 		window.location.replace("/");
+	}
+	// todo: optimize nested try blocks
+	try {
+		await initializeApi();
+		try {
+			portfolioStore.getAllPortfolios();
+		} catch (error) {
+			console.log("get all portfolios error", error);
+		}
+	} catch (error) {
+		console.log("init error", error);
 	}
 });
 
@@ -63,6 +84,35 @@ function goBack() {
 	} else {
 		portfolioStore.activeMode = "create";
 		portfolioStore.reset();
+	}
+	joinView.value = false;
+	createView.value = false;
+}
+
+function redirect() {
+	if (joinView.value) {
+		portfolioStore.activeMode = "join";
+		portfolioStore.activePortfolioType = "My Portfolios";
+		// portfolioStore.myPortfolios.push()
+		portfolioStore.reset();
+	} else {
+		// populate my portfolios array with this new deposit
+		// - if it's a new portfolio you're creating i.e first deposit,
+		// - then push object to my portfolios
+		// - else update existing portfolio with new deposit, rebalance and all
+
+		// if it's a new portfolio you're joining,
+		// simply push object to myPortfolios
+		portfolioStore.activeMode = "create";
+		portfolioStore.firstCreateView = false;
+		portfolioStore.selectedPortfolio = {
+			name: "test display",
+			fee: 0,
+			d7: 0,
+			d30: 0,
+			d90: 0,
+			y1: 0,
+		};
 	}
 	joinView.value = false;
 	createView.value = false;
