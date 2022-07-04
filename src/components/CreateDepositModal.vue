64e35b361daf52a92190c63f939ee54eaec37a72
Dikachi Nze <nzebenflorence@gmail.com>
2022-07-04 14:11:09 +0100

    deposit modal, 2nd create view

--- src/components/CreateDepositModal.vue
+++ src/components/CreateDepositModal.vue
@@ -2,19 +2,27 @@
 	<DepositModal
 		@deposit-action="createDepositAction"
 		@go-back="props.goBack"
+		@redirect="props.redirect"
 	/>
 </template>
 
 <script setup>
 import DepositModal from "./dashboard/DepositModal.vue";
+import { usePortfolios } from "@/stores/Portfolios";
+
+const portfolioStore = usePortfolios();
 
 const props = defineProps({
 	goBack: {
+		type: Function,
+	},
+	redirect: {
 		type: Function,
 	},
 });
 
 function createDepositAction() {
 	console.log("Opened Create Deposit Action");
+	portfolioStore.depositDialog = true;
 }
 </script>
--- src/components/JoinDepositModal.vue
+++ src/components/JoinDepositModal.vue
@@ -1,17 +1,28 @@
 <template>
-	<DepositModal @deposit-action="joinDepositAction" @go-back="props.goBack" />
+	<DepositModal
+		@deposit-action="joinDepositAction"
+		@go-back="props.goBack"
+		@redirect="props.redirect"
+	/>
 </template>
 
 <script setup>
 import DepositModal from "./dashboard/DepositModal.vue";
+import { usePortfolios } from "@/stores/Portfolios";
+
+const portfolioStore = usePortfolios();
 
 const props = defineProps({
 	goBack: {
+		type: Function,
+	},
+	redirect: {
 		type: Function,
 	},
 });
 
 function joinDepositAction() {
 	console.log("Opened Join Deposit Action");
+	portfolioStore.depositDialog = true;
 }
 </script>
