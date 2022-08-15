91f6febe29fbd0aef2a92542f8fa6969c8e47055
Dikachi Nze <nzebenflorence@gmail.com>
2022-08-15 18:51:30 +0100

    portfolio load fix

--- src/components/history/Main.vue
+++ src/components/history/Main.vue
@@ -1,5 +1,28 @@
 <template>
-	<div class="pt-[10px] w-full h-screen">dash main</div>
+	<div>kmdkmdkd</div>
+	<!-- <div class="pt-[10px] w-full h-screen">
+		<div class="flex">
+			<h2 class="text-[20px] text-white">Account History</h2>
+
+			<div class="flex gap-[30px]">
+				<button
+					v-for="(tab, i) in tabs"
+					:key="i"
+					@click="
+									filterActiveHistoryType(i),
+								"
+					class="py-[8px] px-[15px] text-[14px] w-[160px] uppercase cursor-pointer"
+					:class="[
+						portfolioStore.activePortfolioType === tab
+							? 'bg-[#2D3035] text-white'
+							: 'bg-black text-[#868C9D] shadow-[0px_0px_5px_rgba(0,0,0,0.5);]',
+					]"
+				>
+					{{ tab }}
+				</button>
+			</div>
+		</div>
+	</div> -->
 </template>
 
 <script setup>
