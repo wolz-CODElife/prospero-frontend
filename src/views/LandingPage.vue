e3195601711aba6e7517064f59c01045c70bedba
Dikachi Nze <nzebenflorence@gmail.com>
2022-09-19 17:36:34 +0100

    refactor connect wallet

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -12,7 +12,7 @@
 				>Docs</RouterLink
 			>
 			<button
-				@click="toggleWallet"
+				@click="toggleWalletModal()"
 				class="border-2 border-white text-white bg-transparent hover:bg-white hover:text-[#00FF00] uppercase py-[8px] px-[48px]"
 			>
 				Launch App
@@ -29,136 +29,34 @@
 			/>
 
 			<!-- Hero -->
-			<Hero tagline="Profits To The People" @open="toggleWallet()" />
+			<Hero tagline="Profits To The People" @open="toggleWalletModal()" />
 		</div>
 
-		<!-- Connect WalletModal  -->
-		<Modal v-if="walletConnectModal" @close="walletConnectModal = false">
-			<div class="text-white">
-				<!-- Logo  -->
-				<img
-					src="https://i.postimg.cc/tJMqnqDk/image.png"
-					alt=""
-					class="mx-auto w-[85px]"
-				/>
-
-				<!-- Prospero Text -->
-				<img
-					src="@/assets/img/prospero.svg"
-					alt="Prospero Logo Text"
-					class="w-[103px] mx-auto mt-[14px] mb-[28px]"
-				/>
-
-				<p class="text-center text-[16px] mb-[30px]">
-					Please connect your wallet
-				</p>
-
-				<Wallet
-					wallet="Metamask"
-					logo="https://i.postimg.cc/vHLKfmGX/image.png"
-					@connect="connectMetaMaskWallet"
-				/>
-
-				<Wallet
-					wallet="Coinbase Wallet"
-					logo="https://i.postimg.cc/3xBrW7yD/image.png"
-					@connect="useWalletConnect"
-				/>
-
-				<Wallet
-					wallet="Coin 98"
-					logo="https://i.postimg.cc/52V2mJ6n/image.png"
-					@connect="connectCoin98"
-				/>
-
-				<Wallet
-					wallet="Fortmatic"
-					logo="https://i.postimg.cc/nV45DtfH/image.png"
-					@connect="useWalletConnect"
-				/>
-
-				<Wallet
-					wallet="Wallet Connect"
-					logo="https://i.postimg.cc/sXXXZJvz/image.png"
-					@connect="connectWalletConnect"
-				/>
-			</div>
-		</Modal>
+		<!-- Connect Wallet Modal  -->
+		<ConnectWalletModal
+			@connectMetaMask="useConnectMetaMask"
+			@connect-wallet-connect="useWalletConnect"
+			@close-wallet-modal="toggleWalletModal"
+			:is-showing="walletModal"
+		/>
 	</div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import Hero from "@/components/landing/Hero.vue";
-import Modal from "@/components/Modal.vue";
-import Wallet from "../components/landing/Wallet.vue";
-import connect from "../composables/connect/index";
 import AppAlert from "@/components/AppAlert.vue";
+import ConnectWalletModal from "../components/modals/ConnectWalletModal.vue";
+import connect from "../composables/connect/index";
+import useConnectMetaMask from "../composables/connectMetaMask";
 
-const { connectWalletConnect, state } = connect();
+const { connectWalletConnect, err } = connect();
 
-const walletConnectModal = ref(false);
+const walletModal = ref(false);
 
-const err = ref({
-	msg: "",
-	type: "",
-});
-
-function toggleWallet() {
-	walletConnectModal.value = !walletConnectModal.value;
+function toggleWalletModal() {
+	walletModal.value = !walletModal.value;
 }
-
-// TODO:
-//
-// Implement a proper redirect after authentication
-async function connectMetaMaskWallet() {
-	if (typeof window.ethereum !== "undefined") {
-		const accounts = await ethereum.request({
-			method: "eth_requestAccounts",
-		});
-
-		const chainId = await ethereum.request({ method: "eth_chainId" });
-		const account = accounts[0];
-
-		if (account) {
-			state.status = true;
-			state.address = account;
-			state.chainId = chainId;
-			window.location.replace("dashboard");
-		}
-	} else {
-		toggleWallet();
-		err.value = {
-			msg: "Install Metamask",
-			type: "error",
-		};
-	}
-}
-
-async function connectCoin98() {
-	if (window.coin98 || window.ethereum?.isCoin98) {
-		const accounts = await ethereum.request({
-			method: "eth_requestAccounts",
-		});
-
-		const chainId = await ethereum.request({ method: "eth_chainId" });
-		const account = accounts[0];
-
-		if (account) {
-			state.status = true;
-			state.address = account;
-			state.chainId = chainId;
-			window.location.replace("dashboard");
-		}
-	} else {
-		err.value = {
-			msg: "Coin98 Extension is not installed!",
-			type: "error",
-		};
-	}
-}
-
-async function connectCoinbase() {}
 
 async function useWalletConnect() {
 	connectWalletConnect().then((data) => {
