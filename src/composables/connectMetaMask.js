25c8007583f6b82d26c3f8fc427b6a13fd3897fd
Dikachi Nze <nzebenflorence@gmail.com>
2022-09-19 17:32:27 +0100

    componentize

---
+++ src/composables/connectMetaMask.js
@@ -0,0 +1,28 @@
+import connect from "./connect/index";
+const { state, err } = connect();
+
+const connectMetaMask = async () => {
+	if (typeof window.ethereum !== "undefined") {
+		const accounts = await ethereum.request({
+			method: "eth_requestAccounts",
+		});
+
+		const chainId = await ethereum.request({ method: "eth_chainId" });
+		const account = accounts[0];
+
+		if (account) {
+			state.status = true;
+			state.address = account;
+			state.chainId = chainId;
+			window.location.replace("dashboard");
+		}
+	} else {
+		err.value = {
+			msg: "Install Metamask",
+			type: "error",
+		};
+	}
+	return err;
+};
+
+export default connectMetaMask;
