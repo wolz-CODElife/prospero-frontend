2410c04d3b3355d0a3e9cdb15fc937c0d0ec37ca
Dikachi Nze <nzebenflorence@gmail.com>
2022-09-20 16:45:37 +0100

    adjust padding of social links

--- src/components/dashboard/Socials.vue
+++ src/components/dashboard/Socials.vue
@@ -19,34 +19,40 @@
 			</template>
 		</DashDir>
 
-		<hr class="my-[12px] border-[#2D3035]" />
+		<hr class="mt-[12px] border-[#2D3035]" />
 
 		<!-- Bottom  -->
-		<div
-			v-if="portfolioStore.activeMode === 'create'"
-			class="text-center text-[30px] text-white"
-		>
-			New UI here
-		</div>
+		<div class="flex flex-col justify-center">
+			<div
+				v-if="portfolioStore.activeMode === 'create'"
+				class="text-center text-[30px] text-white"
+			>
+				New UI here
+			</div>
 
-		<div
-			v-else
-			class="flex justify-between items-center px-[48px]"
-			:class="
-				props.disabled
-					? 'opacity-30 cursor-text'
-					: 'opacity-1 cursor-pointer'
-			"
-		>
-			<div v-for="(sm, i) in props.socials" :key="i">
-				<div class="flex flex-col items-center gap-y-[12px]">
-					<h2 class="text-[#868C9D] text-[14px] uppercase">
-						{{ sm.title }}
-					</h2>
-					<!-- todo: convert this to read link from API -->
-					<a :href="sm.link">
-						<img :src="sm.icon" alt="" class="h-[40px] object-contain" />
-					</a>
+			<div
+				v-else
+				class="flex justify-between items-center px-[48px] pt-[28px]"
+				:class="
+					props.disabled
+						? 'opacity-30 cursor-text'
+						: 'opacity-1 cursor-pointer'
+				"
+			>
+				<div v-for="(sm, i) in props.socials" :key="i">
+					<div class="flex flex-col items-center gap-y-[12px]">
+						<h2 class="text-[#868C9D] text-[14px] uppercase">
+							{{ sm.title }}
+						</h2>
+						<!-- todo: convert this to read link from API -->
+						<a :href="sm.link">
+							<img
+								:src="sm.icon"
+								alt=""
+								class="h-[40px] object-contain"
+							/>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
