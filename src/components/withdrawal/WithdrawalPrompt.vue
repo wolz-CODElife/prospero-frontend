fdbe1a3a69976c0080bca743bd785e466090afe5
Dikachi Nze <nzebenflorence@gmail.com>
2022-09-20 12:48:30 +0100

    adjust width, height & font size to fit

--- src/components/withdrawal/WithdrawalPrompt.vue
+++ src/components/withdrawal/WithdrawalPrompt.vue
@@ -1,19 +1,19 @@
 <template>
 	<button
-		class="w-full bg-[#2D3035] flex gap-[16px] justify-between items-center text-left p-[20px] mb-[20px]"
+		class="w-full bg-[#2D3035] flex gap-[16px] justify-between items-center text-left px-[20px] mb-[20px]"
 		@click="$emit('withdrawAction')"
 	>
 		<div class="basis-3/4">
-			<h5 class="text-[16px] font-medium text-white uppercase">
+			<h5 class="text-[14px] font-medium text-white uppercase">
 				{{ props.title }}
 			</h5>
-			<p class="text-[#C3C7CD] text-[14px]">{{ props.desc }}</p>
+			<p class="text-[#C3C7CD] text-[12px]">{{ props.desc }}</p>
 		</div>
 
 		<img
 			:src="props.icon"
 			alt=""
-			class="w-[150px] max-h-[90%] object-contain"
+			class="w-[120px] max-h-[80%] object-contain"
 		/>
 
 		<img src="https://i.postimg.cc/L8298gb5/image.png" alt="" />
