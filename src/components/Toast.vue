9cf0709672f08507e7209f98b6dae29994906e8b
Dikachi Nze <nzebenflorence@gmail.com>
2022-09-20 22:59:42 +0100

    create toast component

---
+++ src/components/Toast.vue
@@ -0,0 +1,22 @@
+<template>
+	<!-- convert to toast component eventually: todo  -->
+	<div
+		class="fixed bottom-0 inset-x-0 bg-[#] z-[1000]"
+		:class="[
+			props.toast.type === 'success' ? 'bg-[#40b96e]' : 'bg-[#A96965]',
+		]"
+		v-if="props.toast.isToastActive"
+	>
+		<div class="mx-auto text-center text-white py-[12px]">
+			{{ props.toast.text }}
+		</div>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	toast: {
+		type: Object,
+	},
+});
+</script>
