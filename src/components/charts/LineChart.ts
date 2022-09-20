98b72c2eef8ba13340849f33fdedadc30babfb72
Dikachi Nze <nzebenflorence@gmail.com>
2022-09-20 12:47:03 +0100

    date and height

--- src/components/charts/LineChart.ts
+++ src/components/charts/LineChart.ts
@@ -44,7 +44,7 @@ export default defineComponent({
 		},
 		height: {
 			type: Number,
-			default: 200,
+			default: 150,
 		},
 		cssClasses: {
 			default: "",
@@ -60,19 +60,25 @@ export default defineComponent({
 		},
 	},
 	setup(props) {
-
 		const portfolioStore = usePortfolios();
 		const chartOptions = {
 			responsive: true,
 			maintainAspectRatio: false,
 			onClick: (event, elements, chart) => {
-				if (elements[0]) {            
-				   const i = elements[0].index;
-				   let dateOptions = { month: 'short', day: 'numeric', year: 'numeric' };
-				   let newDate = new Date(chart.data.labels[i]).toLocaleDateString('en-us', dateOptions)
-				   portfolioStore.lineChartSelectedDate = newDate
-				   
-				//    console.log("Clicking Line chart",chart.data.labels[i] + ': ' + chart.data.datasets[0].data[i]);
+				if (elements[0]) {
+					const i = elements[0].index;
+					let dateOptions = {
+						month: "short",
+						day: "numeric",
+						year: "numeric",
+					};
+					let newDate = new Date(chart.data.labels[i]).toLocaleDateString(
+						"en-us",
+						dateOptions
+					);
+					portfolioStore.lineChartSelectedDate = newDate;
+
+					//    console.log("Clicking Line chart",chart.data.labels[i] + ': ' + chart.data.datasets[0].data[i]);
 				}
 			},
 			plugins: {
