dd4160b7903393f672bc610c9342c17c4049368d
Joel Adewole <joeladewole3@gmail.com>
2022-09-21 17:01:18 +0100

    tried out new switch functionality

--- src/composables/tempChangeMetamask.js
+++ src/composables/tempChangeMetamask.js
@@ -1,41 +1,41 @@
-import { ethers } from "ethers";
-import connect from "./connect/index";
-const { state, err } = connect();
+// import { ethers } from "ethers";
+// import connect from "./connect/index";
+// const { state, err } = connect();
 
-const changeMetaMask = async () => {
-    console.log("Switching metamask");
-	if (typeof window.ethereum !== "undefined") {
-        try {
-            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-            // let accounts = await provider.send("eth_requestAccounts", []);
-            // let account = accounts[0];
-            // console.log("First address", account);
-            provider.on("disconnect")
-            await provider.send("eth_requestAccounts", []);
+// const changeMetaMask = async () => {
+//     console.log("Switching metamask");
+// 	if (typeof window.ethereum !== "undefined") {
+//         try {
+//             const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+//             // let accounts = await provider.send("eth_requestAccounts", []);
+//             // let account = accounts[0];
+//             // console.log("First address", account);
+//             provider.on("disconnect")
+//             await provider.send("eth_requestAccounts", []);
     
-            provider.on('accountsChanged', function (accounts) {
-                let account = accounts[0];
-                console.log(account);
-                if (account) {
-                    state.status = true;
-                    state.address = account;
-                }
-            });
-            const signer = provider.getSigner();
-            const address = await signer.getAddress();
+//             provider.on('accountsChanged', function (accounts) {
+//                 let account = accounts[0];
+//                 console.log(account);
+//                 if (account) {
+//                     state.status = true;
+//                     state.address = account;
+//                 }
+//             });
+//             const signer = provider.getSigner();
+//             const address = await signer.getAddress();
 
-            console.log(address);
+//             console.log(address);
     
-        } catch(error) {
+//         } catch(error) {
 
-        }
-	} else {
-		err.value = {
-			msg: "Install Metamask",
-			type: "error",
-		};
-	}
-	return err;
-};
+//         }
+// 	} else {
+// 		err.value = {
+// 			msg: "Install Metamask",
+// 			type: "error",
+// 		};
+// 	}
+// 	return err;
+// };
 
-export default changeMetaMask;
+// export default changeMetaMask;
